d62e3ccbbaefd18b6ff7f69b42150e69e1a3ebdc
Peter André Altier <petera113.pa@gmail.com>
2025-01-30 11:48:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+        function redLightOn() {
+            document.getElementById("rød").classList.add("red");
+            document.getElementById("gul").classList.remove("yellow");
+            document.getElementById("grønn").classList.remove("green");
+        }
+
+        function yellowLightOn() {
+            document.getElementById("rød").classList.remove("red");
+            document.getElementById("gul").classList.add("yellow");
+            document.getElementById("grønn").classList.remove("green");
+        }
+
+        function greenLightOn() {
+            document.getElementById("rød").classList.remove("red");
+            document.getElementById("gul").classList.remove("yellow");
+            document.getElementById("grønn").classList.add("green");
+        }
+        yellowAndRedLightOn = function() {
+            document.getElementById("rød").classList.add("red");
+            document.getElementById("gul").classList.add("yellow");
+            document.getElementById("grønn").classList.remove("green");
+        }
+
+        function intervall() {
+          
+            setInterval(function() {
+                redLightOn();  
+                setTimeout(yellowAndRedLightOn, 1000);  
+                setTimeout(greenLightOn, 3000); 
+            }, 6000);
+        }
+
+window.onload = intervall;  
